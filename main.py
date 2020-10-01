76f185912fb6860d009de92a1b52ee9c99c2a8e0
PalPalash <64191803+PalPalash@users.noreply.github.com>
2020-10-01 22:36:49 +0530

    Added Stuff

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 # Importing the API Module . Use pip install wikipedia to download and install it.
+# I have commented out stuff which I dont need , but you might need it just remove # and it will be a normal function
 import wikipedia 
+import webbrowser
+import time 
+
 
 f = open("output_from_wikibot.txt", "w")
+print("Enter Search Term:")
 search_about = input() # Getting The Input
+print("Getting Data ... Please wait .... see the output_from_wikibot.txt for data as well")
+
+
 # Lenght Of Descriptions
 short_description = wikipedia.summary(search_about , sentences= 3)
 medium_description = wikipedia.summary(search_about, sentences=15)
@@ -20,28 +28,41 @@ summary = pageonsearch.summary
 title = pageonsearch.title
 
 
-
-
-# Writing The Result
+# Printing The Result
 print("Page content:\n", content, "\n")
 print("Page title:", title, "\n")
 print("Categories:", categories, "\n")
-print("Links:", links, "\n")
-print("References:", references, "\n")
+#print("Links:", links, "\n")
+#print("References:", references, "\n")
 print("Summary:", summary, "\n")
-# Or You Can Print these : (Just remove hashtags in front of them and in put hashtags in front of "with open" , code obove this and evrything under it )
+
+
+# Or You Can Print these : (Just remove hashtags in front of them and in put hashtags in front of "with open" 👇 , code obove this 👆 and evrything under it )
 #print(medium_description)
 #print(long_description)
 #print(short_description)
 
 
-
-
-#print(result)
+# Writing To /output_from_wikibot.txt 
 with open("output_from_wikibot.txt", "w" , encoding='utf-8') as f:
     f.write("Page content:\n" + content + "\n")
     f.write("Page title:" + title + "\n")
-    f.write("Categories:" + categories + "\n")
-    f.write("Links:" + links + "\n")
-    f.write("References:" + references + "\n")
+    #f.write("Categories:" + categories + "\n")
+    #f.write("Links:" + links + "\n")
+    #f.write("References:" + references + "\n")
     f.write("Summary:" + summary + "\n")
+
+
+
+
+
+## Please subscribe to my YouTube channel 
+my_channel = "PalPalash"
+print("Please subscribe to my channel " + my_channel + " on Youtube")
+
+webbrowser.open('https://www.youtube.com/channel/UCdfaHl9USu-J-kp4Bj_7J2Q?sub_confirmation=1', new=2)
+
+
+print("Quitting Now")
+quit()
+
