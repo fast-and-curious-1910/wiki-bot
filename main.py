a8b3151fa7e4a968d564196d2a5ea13f06d600cd
PalPalash <64191803+PalPalash@users.noreply.github.com>
2020-10-01 13:12:39 +0530

    debugging

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import wikipedia 
 
-f = open("output_from_wikibot", "w")
+f = open("output_from_wikibot.txt", "w")
 search_about = input()
 short_description = wikipedia.summary(search_about , sentences= 3)
 medium_description = wikipedia.summary(search_about, sentences=15)
@@ -21,12 +21,12 @@ title = pageonsearch.title
 
 
 # Writing The Result
-print("Page content:\n", content, "\n")
-print("Page title:", title, "\n")
-print("Categories:", categories, "\n")
-print("Links:", links, "\n")
-print("References:", references, "\n")
-print("Summary:", summary, "\n")
+#print("Page content:\n", content, "\n")
+#print("Page title:", title, "\n")
+#print("Categories:", categories, "\n")
+#print("Links:", links, "\n")
+#print("References:", references, "\n")
+#print("Summary:", summary, "\n")
 
 
 
@@ -35,10 +35,10 @@ print("Summary:", summary, "\n")
 
 #print(result)
 #print(short_description)
-#f.write("Page content:\n", content, "\n")
-#f.write("Page title:", title, "\n")
-#f.write("Categories:", categories, "\n")
-#f.write("Links:", links, "\n")
-#f.write("References:", references, "\n")
-#f.write("Summary:", summary, "\n")
-
+with open("output_from_wikibot.txt", "w" , encoding='utf-8') as f:
+    f.write("Page content:\n" + content + "\n")
+    f.write("Page title:" + title + "\n")
+    f.write("Categories:" + categories + "\n")
+    f.write("Links:" + links + "\n")
+    f.write("References:" + references + "\n")
+    f.write("Summary:" + summary + "\n")
